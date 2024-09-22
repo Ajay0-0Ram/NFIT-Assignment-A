62ada5f371ab461b74eb196b001e9a5f8eb4067a
Ajay0-0Ram <ajayramsaran2012@gmail.com>
2024-09-22 15:25:19 -0400

    Update Main.java

--- Number System Converter/src/Main.java
+++ Number System Converter/src/Main.java
@@ -12,10 +12,32 @@ public class Main{
         double input_number;
 
         //getting the users input (Input number, source base, and target base)
-        System.out.println("What is your source base (2: Binary, 10: Decimal, 16: Hexadecimal): ");
+        System.out.println("**Welcome to our Number System Converter**\nHere you can convert any digit (including negative and fractional) from one of our approved number systems to another!\n\n**NOTE any invalid input will be disregarded");
 
+        //getting source base
+        System.out.println("What is your source base (2: Binary, 10: Decimal, 16: Hexadecimal): ");
         do{
             source_base = sc.nextInt();
         }while(source_base != 2 && source_base != 6 && source_base != 16);
+
+        //getting target base
+        System.out.println("What is your target base (2: Binary, 10: Decimal, 16: Hexadecimal): ");
+        do{
+            target_base = sc.nextInt();
+        }while(target_base != 2 && target_base != 6 && target_base != 16);
+
+        //getting input number
+        System.out.println("What is your number: ");
+        do{
+            input_number = sc.nextDouble();
+        }while(!ValidateInput(input_number));
+
+    }
+
+    //Description: Validates the users number, checks if the number given adheres to the restrictions of their source_base
+    //Parameters: the users number
+    //Return: returns true if the users number adheres to the source, and false otherwise
+    public static boolean ValidateInput(Double input_number){
+        return false;
     }
 }
