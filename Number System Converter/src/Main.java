b0f342f9e10b8de2eb9ae3a823bddbad13144d17
Ajay0-0Ram <ajayramsaran2012@gmail.com>
2024-09-23 18:13:19 -0400

    Update Main.java

--- Number System Converter/src/Main.java
+++ Number System Converter/src/Main.java
@@ -37,7 +37,11 @@ public class Main{
     //Description: Validates the users number, checks if the number given adheres to the restrictions of their source_base
     //Parameters: the users number, and the source_base
     //Return: returns true if the users number adheres to the source, and false otherwise
-    public static boolean ValidateInput(Double input_number){
+    public static boolean ValidateInput(double input_number, int source_base){
         return false;
+    }
+
+    public static double convert_number(double input_number, int source_base, int target_base){
+        return 0.0;
     }
 }
