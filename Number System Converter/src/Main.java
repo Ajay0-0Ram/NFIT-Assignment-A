a4e1b0def0e69d52671e54c9562dad96a85dcbfa
SameerKarodia <sameerkarodia@gmail.com>
2024-09-29 14:39:42 -0400

    Update Main.java

--- Number System Converter/src/Main.java
+++ Number System Converter/src/Main.java
@@ -36,14 +36,42 @@ public class Main{
             input_number = sc.next();
             if (ValidateInput(input_number, source_base)) {
                 // After the input is validated, print confirmation
-                System.out.println("Valid input number");
+                System.out.println("valid input number");
                 break;  // Valid input number, exit loop
             }
             System.out.println("Invalid input number. Please try again.");
         }
 
 
+        //GETTING AN ARRAY OF THE INPUT NUMBER PROVIDED
+        ArrayList<Integer>Input_array=digitSplit(input_number);
+        //TESTING ARRAY
+        System.out.println("Full array is: ");
+        for (int i=0;i<Input_array.size();i++){
+            System.out.print(Input_array.get(i)+" ");
 
+        }
+
+        //INTEGER PART OF INPUT NUMBER
+        ArrayList<Integer>Input_array_integers=digitSplitInteger(Input_array);
+        //TESTING ARRAY
+        System.out.print("\nIntegers of array are: ");
+        for (int i=0;i<Input_array_integers.size();i++){
+            System.out.print(Input_array_integers.get(i)+" ");
+
+        }
+
+        //FRACTION PART OF INPUT NUMBER
+        ArrayList<Integer>Input_array_fraction=digitSplitFraction(Input_array);
+        //TESTING ARRAY
+        System.out.print("\nFraction of array is: ");
+        for (int i=0;i<Input_array_fraction.size();i++){
+            System.out.print(Input_array_fraction.get(i)+" ");
+
+        }
+
+        double test=convert_number(input_number,source_base,target_base,Input_array_integers,Input_array_fraction);
+        System.out.print("\nDecimal value is: "+test);
 
     }
 
@@ -52,7 +80,11 @@ public class Main{
     //Parameters: input_number and source_base
     //Return: returns true if the users number adheres to the source, and false otherwise
     public static boolean ValidateInput(String input_number, int source_base){
+
         String valid_nums="";
+        //Flag to make sure only 1 negative number and 1 decimal point can be used
+        boolean hasDecimal_point=false;
+        boolean hasNegative_sign=false;
 
         // Define valid characters based on the source_base given
         if(source_base == 2){
@@ -72,7 +104,21 @@ public class Main{
         for (int i=0; i<input_number.length(); i++){
             char z=input_number.charAt(i);
 
-            if(valid_nums.indexOf(z)==-1){ //checking to see if the character is valid for source_base
+            //checking ONLY first value for a negative sign
+            if(z=='-' && i==0 && !hasNegative_sign){
+                hasNegative_sign=true;
+                continue;
+            }
+
+            //if character is a decimal then changes flag to not allow anymore decimals
+            if(z=='.' && !hasDecimal_point){
+                hasDecimal_point=true;
+                continue;
+            }
+
+
+            //checking to see if the character is valid for source_base
+            if(valid_nums.indexOf(z)==-1){
                 return false;
             }
         }
@@ -83,13 +129,75 @@ public class Main{
     //Descripton: computes the conversion of the input_number from  source_base to target_base
     //Parameters: input_number, source_base and target_base
     //Return: the input_number in the target_base
-    public static double convert_number(String input_number, int source_base, int target_base){
-        //code to convert from source_base to decimal
+    public static double convert_number(String input_number, int source_base, int target_base,ArrayList<Integer> Input_array_integers,ArrayList<Integer> Input_array_fraction){
+        double decimal_int_value = 0;
+        double decimal_fraction_value=0;
+        double decimal_value=0;
+
+        //code to convert from source_base to decimal//
+        //Binary to Decimal//
+        if (source_base==2){
+            int power=0;
+
+            //converting integer part
+            for(int i=Input_array_integers.size()-1; i>0; i--){   //Starting from last digit in the integer part and stopping before the first number due to it representing negative or not//
+                double currentDigit=Input_array_integers.get(i);
+                decimal_int_value+=currentDigit*(Math.pow(2,power));
+                power++;    //Add 1 to power each time to move up the multiplication//
+            }
+
+            //converting fraction part
+            //change power to negative for fractions
+            power=-1;
+            for (int i=0;i<Input_array_fraction.size();i++){  //Starting from first digit in fraction part and multiplying by negative power
+                double currentDigit=Input_array_fraction.get(i);
+                decimal_fraction_value+=currentDigit*(Math.pow(2,power));
+                power--;    //Add 1 to power each time to move up the multiplication//
+
+            }
+
+            //applying negative value if negative sign is found
+            if(Input_array_integers.get(0)==-1){
+                decimal_int_value=decimal_int_value*-1;
+            }
+
+            //add up both the integer and fraction part together
+            decimal_value=decimal_fraction_value+decimal_int_value;
+
+        }
+
+        //Hexadecimal to Decimal//
+        if (source_base==16){
+            int power=0;
+
+            //converting integer part
+            for(int i=Input_array_integers.size()-1; i>0; i--){   //Starting from last digit in the input number and stopping before the first number due to it representing negative or not//
+                double currentDigit=Input_array_integers.get(i);
+                decimal_int_value+=currentDigit*(Math.pow(16,power));
+                power++;    //Add 1 to power each time to move up the multiplication//
+            }
+
+            //converting fraction part
+            //change power to negative for fractions
+            power=-1;
+            for (int i=0;i<Input_array_fraction.size();i++){  //Starting from first digit in fraction part and multiplying by negative power
+                double currentDigit=Input_array_fraction.get(i);
+                decimal_fraction_value+=currentDigit*(Math.pow(16,power));
+                power--;    //Add 1 to power each time to move up the multiplication//
+
+            }
+
+            //applying negative value if negative sign is found
+            if(Input_array_integers.get(0)==-1){
+                decimal_int_value=decimal_int_value*-1;
+            }
+
+            decimal_value=decimal_int_value+decimal_fraction_value;
+        }
 
 
-
-        //code to convert form decimal to target_base
-        return 0.0;
+        //returning to convert form source to target_base
+        return decimal_value;
     }
 
     //Descripton: splits the input_number into its digits, and stores them in an array of integers,
@@ -132,7 +240,41 @@ public class Main{
 
 
     }
+    public static ArrayList<Integer> digitSplitInteger(ArrayList<Integer> Input_array) {
+        ArrayList<Integer> integerPart = new ArrayList<>();
 
+        // Goes through the input array and spits out everything before the decimal point (100)
+        for (int digit : Input_array) {
+
+            if (digit == 100) {
+                break;  // Stop at the decimal point
+            }
+            integerPart.add(digit);  // Add digits before the decimal point
+        }
+
+        return integerPart;
+    }
+
+    public static ArrayList<Integer> digitSplitFraction(ArrayList<Integer> Input_array) {
+        ArrayList<Integer> fractionalPart = new ArrayList<>();
+        //flag for checking if found a decimal
+        boolean foundDecimalPoint=false;
+
+        // Going through each value in the input array
+        for (int digit : Input_array) {
+            // Once it sees (100), starts adding the digits after it
+            if (digit == 100) {
+                foundDecimalPoint = true;
+                continue;  // Skip the decimal point (100) itself
+            }
+
+            if (foundDecimalPoint) {
+                fractionalPart.add(digit);  // Add all digits after the decimal point
+            }
+        }
+
+        return fractionalPart;
+    }
 
 }
 
